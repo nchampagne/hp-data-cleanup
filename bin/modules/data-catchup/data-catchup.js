c0bdb018bae4d0d72b9c78136b99de5b712fc405
Nick Champagne <nchampagne@SC4363.local>
2018-10-30 12:00:05 -0400

    Updated migration logic. Added logging. General code cleanup

--- bin/modules/data-catchup/data-catchup.js
+++ bin/modules/data-catchup/data-catchup.js
@@ -1,9 +1,9 @@
-const Mongo	    = require('mongodb').MongoClient;
-const FS 	    = require('fs');
-const Readline  = require('readline');
-const Util      = require("../utils/utils.js");
+const Mongo	        = require('mongodb').MongoClient;
+const FS 	        = require('fs');
+const LineReader    = require('line-by-line')
+const Util          = require("../utils/utils.js");
 
-const Options 	= {
+const Options = {
     useNewUrlParser:	true,
 	auto_reconnect: 	true,
 	keepAlive: 		    1,
@@ -11,48 +11,62 @@ const Options 	= {
 	socketTimeoutMS: 	10000000
 }
 
-let mongoClient	= null;
+let mongoSrcClient	= null;
+let mongoDestClient = null;
+let readLine        = null;
+let logStream       = null;
 
 // Exported hook
 module.exports.execute = function execute(srcMongoUri, destinationMongoUri) {
     createLogDir();
     mongo(srcMongoUri, (srcClient) => {
-        let srcCollection = srcClient.db("health-profile").collection("demographics");
+        mongoSrcClient = srcClient;
+        let srcCollection = mongoSrcClient.db("health-profile").collection("demographics");
         mongo(destinationMongoUri, (destinationClient) => {
-            let destinationCollection = destinationClient.db("health-profile").collection("demographics");
-            readFile(__dirname + "/demographics-secureIds.txt", (secureId) => {
-                if(!secureId) finish(srcClient, destinationClient);
-                else {
-                    srcCollection.findOne({"secureId": secureId}, (err, srcDoc) => {
-                        if(err) console.log("SOURCE COLLECTION ERROR: " + err);
-                        else if(srcDoc) {
-                            destinationCollection.findOne({"secureId": secureId}, (err, destDoc) => {
-                                if(err) console.log("DESTINATION COLLECTION ERROR: " + err);
-                                else {
-                                    if(destDoc) {
-                                        console.log("SecureId found in destination collection. No work necessary!");
-                                    }
-                                    else {
-                                        console.log("Inserting secureId: " + secureId);
-                                        destinationCollection.insertOne(srcDoc, null, (err, result) => {
-                                            if(err) console.log("ERROR INSERTING: " + err);
-                                        });
-                                    } 
-                                }
-                            });
-                        }
-                    });
-                }
+            mongoDestClient = destinationClient;
+            let destinationCollection = mongoDestClient.db("health-profile").collection("demographics");
+            logStream = FS.createWriteStream("logs/data-catchup/demographics.log", { flags: "a" });
+            initReadLine(__dirname + "/demographics-secureIds.txt", (secureId) => {
+                if(!secureId || secureId === "") finish("COMPLETE!", false);
+                else processDoc(srcCollection, destinationCollection, secureId);
             });
         });
     });
 }
 
-function readFile(filename, action) {
-    Readline.createInterface({
-        input: FS.createReadStream(filename),
-        crlfDelay: Infinity
-    }).on("line", (line) => { action(line) });
+function query(secureId) { return { "secureId": secureId }; }
+
+function processDoc(srcColl, destColl, secureId) {
+    console.log("Processing " + secureId);
+    srcColl.findOne(query(secureId), (err, srcDoc) => {
+        if(err) { console.log(errorText("SOURCE COLLECTION ERROR: ") + err); return; }
+        else if(srcDoc) {
+            destColl.findOne(query(secureId), (err, destDoc) => {
+                if(err) { console.log(errorText("DESTINATION COLLECTION ERROR: ") + err); return; }
+                else {
+                    if(destDoc) {
+                        logStream.write("Secure ID found in dest coll: " + secureId + "\n");
+                    }
+                    else {
+                        destColl.insertOne(srcDoc, null, (err, result) => {
+                            if(err) {
+                                logStream.write("Error inserting: " + srcDoc._id + err + "\n");
+                            }
+                            else logStream.write("Inserting: " + srcDoc._id + "\n");
+                        });
+                    } 
+                }
+                readLine.resume();
+            });
+        }
+       else { readLine.resume(); }
+    });
+}
+
+function initReadLine(filename, action) {
+    readLine = new LineReader(filename);
+    readLine.on("error", (err) => { console.log(err); finish("Error", true); });
+    readLine.on("line", (line) => { readLine.pause(); action(line); });
 }
 
 function createLogDir() {
@@ -67,10 +81,11 @@ function createLogDir() {
 function mongo(dbUrl, action) {
     Mongo.connect(dbUrl, Options, (err, client) => {
     	if(err) {
-    		console.log("ERROR: " + err);
-    		return;
+    		console.log(errorText("ERROR: ") + err);
+    		finish("Error", true);
+            return;
     	}
-        console.log("Connected successfully to source server " + dbUrl);
+        console.log(okayText("Connected successfully to mongo host: ") + dbUrl);
         action(client);
     });
 }
@@ -83,12 +98,17 @@ function queryMongo(mongo) {
     }
 }
 
-function finish(mongoSrcClient, mongoDestClient) {
-    Util.prettifyText("ALL DONE!", (text) => {
-		console.log(Util.colors.green + text + Util.colors.nc);
-		mongoSrcClient.close();
-        mongoDestClient.close();
+function finish(text, isError) {
+    Util.prettifyText(text, (text) => {
+        if (isError) console.log(errorText(text));
+        else console.log(okayText(text));
+        if (readLine) readLine.close();
+        if (mongoSrcClient) mongoSrcClient.close();
+        if (mongoDestClient) mongoDestClient.close();
         process.exit(0);
 	});
 }
+
+function errorText(text) { return Util.colors.red + text + Util.colors.nc; }
+function okayText(text) { return Util.colors.green + text + Util.colors.nc; }
 
